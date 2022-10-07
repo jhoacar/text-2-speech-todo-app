b8d75ca7e1ea30aa5be77c384ac67ad826d58d59
jhoacar <carrerojhoan@gmail.com>
2022-10-06 21:23:07 -0300

    added handler to other routes in express router

--- server/src/routes/web/index.js
+++ server/src/routes/web/index.js
@@ -6,4 +6,6 @@ const client = require('../../client.dir');
 
 router.use(express.static(client));
 
+router.use('/*', (req, res) => res.sendFile(`${client}/index.html`));
+
 module.exports = router;
